03d571592ee1322be5b1e944bbf5bf426977efa7
cihat <cihat02020202@gmail.com>
2021-08-25 21:51:41 +0300

    Add subscriber model

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,19 +1,39 @@
 const express = require("express")
 const router = express.Router()
+const Subscriber = require("../models/subscriber")
 
 //* Get all subscribers
-router.get("/", function (req, res, next) {})
+router.get("/", async (req, res, next) => {
+  try {
+    const subscribers = await Subscriber.find()
+    res.json(subscribers)
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+})
 
 //* Get all subscribers
-router.get("/:id", function (req, res, next) {})
+router.get("/:id", async (req, res, next) => {})
 
 //* Creat one subscribers
-router.post("/", function (req, res, next) {})
+router.post("/", async (req, res, next) => {
+  const subscriber = new Subscriber({
+    name: req.body.name,
+    subscribedChannel: req.body.subscribedChannel,
+  })
+
+  try {
+    const newSubscriber = await subscriber.save()
+    res.status(201).json(newSubscriber)
+  } catch (err) {
+    res.status(400).json({ message: err.message })
+  }
+})
 
 //* Update one subscribers
-router.patch("/:id", function (req, res, next) {})
+router.patch("/:id", async (req, res, next) => {})
 
 //* Delete one subscribers
-router.delete("/:id", function (req, res, next) {})
+router.delete("/:id", async (req, res, next) => {})
 
 module.exports = router
