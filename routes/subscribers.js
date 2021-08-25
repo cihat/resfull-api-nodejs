ff1ed8e66435945b253d384d9a40a6ac8d68d881
cihat <cihat02020202@gmail.com>
2021-08-26 00:17:00 +0300

    Add delete, getSubscriber func, get one func, patch

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -2,6 +2,20 @@ const express = require("express")
 const router = express.Router()
 const Subscriber = require("../models/subscriber")
 
+// Middleware function for gettig subscriber object by ID
+async function getSubscriber(req, res, next) {
+  try {
+    subscriber = await Subscriber.findById(req.params.id)
+    if (subscriber == null)
+      return res.status(404).json({ message: "Cant find subscriber" })
+  } catch (err) {
+    return res.status(500).json({ message: err.message })
+  }
+
+  res.subscriber = subscriber
+  next()
+}
+
 //* Get all subscribers
 router.get("/", async (req, res, next) => {
   try {
@@ -12,8 +26,10 @@ router.get("/", async (req, res, next) => {
   }
 })
 
-//* Get all subscribers
-router.get("/:id", async (req, res, next) => {})
+//* Get one subscribers
+router.get("/:id", getSubscriber, async (req, res, next) => {
+  res.json(res.subscriber)
+})
 
 //* Creat one subscribers
 router.post("/", async (req, res, next) => {
@@ -31,9 +47,33 @@ router.post("/", async (req, res, next) => {
 })
 
 //* Update one subscribers
-router.patch("/:id", async (req, res, next) => {})
+router.patch("/:id", getSubscriber, async (req, res, next) => {
+  if (req.body.name != null) {
+    res.subscriber.name = req.body.name
+  }
+
+  if (req.body.subscribedChannel != null) {
+    res.subscriber.subscribedChannel = req.body.subscribedChannel
+  }
+
+  try {
+    const updatedSubscriber = await res.subscriber.save()
+    res.json(updatedSubscriber)
+  } catch (err) {
+    res.status(400).json({ message: err.message })
+  }
+})
 
 //* Delete one subscribers
-router.delete("/:id", async (req, res, next) => {})
+router.delete("/:id", getSubscriber, async (req, res, next) => {
+  try {
+    await res.subscriber.remove()
+    res.json({
+      message: `The subscriber named ${res.subscriber.name} has been deleted...`,
+    })
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+})
 
 module.exports = router
